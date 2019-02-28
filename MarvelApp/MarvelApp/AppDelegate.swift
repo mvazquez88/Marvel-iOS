2e52195f0dcec96061a42927234ad2d6a32a0043
Marcelo Vazquez <Marce88.MDP@gmail.com>
2019-02-28 20:22:27 +0100

    Add Splash Screen images

--- MarvelApp/MarvelApp/AppDelegate.swift
+++ MarvelApp/MarvelApp/AppDelegate.swift
@@ -13,7 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let splitViewController = window!.rootViewController as! UISplitViewController
